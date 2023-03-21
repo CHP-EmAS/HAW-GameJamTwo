6a9f6bcbcc50f91ab4bbb4d45566c80a8b1555b1
NilsBeeeh <128372735+NilsBeeeh@users.noreply.github.com>
2023-03-21 15:49:20 +0100

    Main Menu

--- HAW Game Jam 2/Assets/Packages/Plum/Plum Base/ServiceLocator.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//v Reference Manager!
-public static partial class ServiceLocator
-{
-    public static Camera mouseCamera;
-}
---
+++ HAW Game Jam 2/Assets/UI usw/MainMenu/MainMenuButtons.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    [SerializeField] GameObject MainPanel;
+    [SerializeField] GameObject SettingsPanel;
+    [SerializeField] GameObject CreditsPanel;
+
+    private void Start()
+    {
+        SettingsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        MainPanel.SetActive(true);
+    }
+    public void StartGame()
+    {
+        return;
+    }
+
+    public void OpenSettings(TMP_Text buttonText)
+    {
+        buttonText.fontStyle = FontStyles.Normal;
+
+        MainPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+    }
+
+    public void OpenCredits(TMP_Text buttonText)
+    {
+        buttonText.fontStyle = FontStyles.Normal;
+
+        MainPanel.SetActive(false);
+        CreditsPanel.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMenu(TMP_Text buttonText)
+    {
+        buttonText.fontStyle = FontStyles.Normal;
+
+        SettingsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+        MainPanel.SetActive(true);
+    }
+
+    public void HoverOverButton(TMP_Text hoverButton)
+    {
+        hoverButton.fontStyle = FontStyles.Bold;
+    }
+
+    public void HoverExitButton(TMP_Text hoverButton)
+    {
+        hoverButton.fontStyle = FontStyles.Normal;
+    }
+}
